886e6c3fdc788e31d4a99d80e5fbf01a250ad57f
Amit Prakash <34869115+iamitprakash@users.noreply.github.com>
2025-12-07 21:33:02 +0530

    Implement WorkerPool for concurrent job processing

---
+++ pool.go
@@ -0,0 +1,31 @@
+package worker
+
+import (
+    "yourapp/queue"
+)
+
+type WorkerPool struct {
+    workers int
+    jobs    chan queue.Job
+}
+
+func NewPool(n int) *WorkerPool {
+    return &WorkerPool{
+        workers: n,
+        jobs:    make(chan queue.Job, n),
+    }
+}
+
+func (p *WorkerPool) Start(handle func(queue.Job)) {
+    for i := 0; i < p.workers; i++ {
+        go func() {
+            for job := range p.jobs {
+                handle(job)
+            }
+        }()
+    }
+}
+
+func (p *WorkerPool) Submit(j queue.Job) {
+    p.jobs <- j
+}
